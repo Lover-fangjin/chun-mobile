58f0b063b34e304c106f508368491f98399d712b
fangjin <1063345449@qq.com>
2017-11-09 11:34:34 +0800

    chun

---
+++ src/js/index.js
@@ -0,0 +1,15 @@
+
+import $ from 'jquery';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+
+import 'Swiper/dist/css/swiper.css';
+import  Swiper from 'Swiper/dist/js/swiper.js';
+
+
+
+import '../css/index.less';
+
+
+
+
